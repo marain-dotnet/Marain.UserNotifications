f251dd5b4524e5bce62429be6887d7bb0a81102d
Jonathan George <jon@jonathangeorge.co.uk>
2020-07-31 00:10:40 +0100

    Add basic specs for swagger retrieval.

---
+++ Solutions/Marain.UserNotifications.Specs/Bindings/SelfHostedApiBindings.cs
@@ -0,0 +1,54 @@
+﻿// <copyright file="SelfHostedApiBindings.cs" company="Endjin Limited">
+// Copyright (c) Endjin Limited. All rights reserved.
+// </copyright>
+
+namespace Marain.UserNotifications.Specs.Bindings
+{
+    using System.Threading.Tasks;
+    using Corvus.Testing.SpecFlow;
+    using Menes.Testing.AspNetCoreSelfHosting;
+    using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.Extensions.Logging;
+    using TechTalk.SpecFlow;
+
+    [Binding]
+    public static class SelfHostedApiBindings
+    {
+        [BeforeScenario("useManagementApi", Order = ContainerBeforeScenarioOrder.ServiceProviderAvailable)]
+        public static Task StartSelfHostedManagementApi(ScenarioContext scenarioContext)
+        {
+            var hostManager = new OpenApiWebHostManager();
+            scenarioContext.Set(hostManager);
+
+            return hostManager.StartHostAsync<Management.Host.Startup>(
+                "http://localhost:7080",
+                services =>
+                {
+                    // Ensure log level for the service is set to debug.
+                    services.AddLogging(builder => builder.SetMinimumLevel(LogLevel.Debug));
+                });
+        }
+
+        [BeforeScenario("useApiDeliveryChannelApi", Order = ContainerBeforeScenarioOrder.ServiceProviderAvailable)]
+        public static Task StartSelfHostedApiDeliveryChannelApi(ScenarioContext scenarioContext)
+        {
+            var hostManager = new OpenApiWebHostManager();
+            scenarioContext.Set(hostManager);
+
+            return hostManager.StartHostAsync<ApiDeliveryChannel.Host.Startup>(
+                "http://localhost:7080",
+                services =>
+                {
+                    // Ensure log level for the service is set to debug.
+                    services.AddLogging(builder => builder.SetMinimumLevel(LogLevel.Debug));
+                });
+        }
+
+        [AfterScenario]
+        public static Task StopSelfHostedApi(ScenarioContext scenarioContext)
+        {
+            OpenApiWebHostManager hostManager = scenarioContext.Get<OpenApiWebHostManager>();
+            return hostManager.StopAllHostsAsync();
+        }
+    }
+}
---
+++ Solutions/Marain.UserNotifications.Specs/Steps/ApiSteps.cs
@@ -0,0 +1,58 @@
+﻿// <copyright file="ApiSteps.cs" company="Endjin Limited">
+// Copyright (c) Endjin Limited. All rights reserved.
+// </copyright>
+
+namespace Marain.UserNotifications.Specs.Steps
+{
+    using System;
+    using System.Net;
+    using System.Net.Http;
+    using System.Threading.Tasks;
+    using Newtonsoft.Json.Linq;
+    using NUnit.Framework;
+    using TechTalk.SpecFlow;
+
+    [Binding]
+    public class ApiSteps
+    {
+        private const string ResponseContent = "ApiSteps:ResponseContent";
+
+        private static readonly HttpClient HttpClient = HttpClientFactory.Create();
+
+        private static readonly Uri BaseUri = new Uri("http://localhost:7080");
+
+        private readonly ScenarioContext scenarioContext;
+
+        public ApiSteps(ScenarioContext scenarioContext)
+        {
+            this.scenarioContext = scenarioContext;
+        }
+
+        [When("I request the Swagger definition for the API")]
+        public Task WhenIRequestTheSwaggerDefinitionForTheAPI()
+        {
+            return this.SendGetRequest("/swagger");
+        }
+
+        [Then("the response status code should be '(.*)'")]
+        public void ThenTheResultStatusCodeShouldBe(HttpStatusCode expectedStatusCode)
+        {
+            HttpResponseMessage response = this.scenarioContext.Get<HttpResponseMessage>();
+            Assert.AreEqual(expectedStatusCode, response.StatusCode);
+        }
+
+        private async Task SendGetRequest(string path)
+        {
+            HttpResponseMessage response = await HttpClient.GetAsync(new Uri(BaseUri, path)).ConfigureAwait(false);
+
+            this.scenarioContext.Set(response);
+
+            string content = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+
+            if (!string.IsNullOrEmpty(content))
+            {
+                this.scenarioContext.Set(content, ResponseContent);
+            }
+        }
+    }
+}
