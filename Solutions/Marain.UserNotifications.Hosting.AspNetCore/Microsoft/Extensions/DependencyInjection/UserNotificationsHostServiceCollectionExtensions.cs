f8ae126bd1a3f166e6884ec9660e86e4a70f16d0
Jonathan George <jon@jonathangeorge.co.uk>
2020-07-30 23:09:06 +0100

    Add host function startup and entry points.

---
+++ Solutions/Marain.UserNotifications.Hosting.AspNetCore/Microsoft/Extensions/DependencyInjection/UserNotificationsHostServiceCollectionExtensions.cs
@@ -0,0 +1,34 @@
+﻿// <copyright file="UserNotificationsHostServiceCollectionExtensions.cs" company="Endjin Limited">
+// Copyright (c) Endjin Limited. All rights reserved.
+// </copyright>
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    /// <summary>
+    /// Configures the user notifications API hosts.
+    /// </summary>
+    public static class UserNotificationsHostServiceCollectionExtensions
+    {
+        /// <summary>
+        /// Configures services neededed to provide the user notifications management API.
+        /// </summary>
+        /// <param name="services">The service collection.</param>
+        /// <returns>The service collection, to enable chaining.</returns>
+        public static IServiceCollection AddTenantedUserNotificationsManagementApi(
+            this IServiceCollection services)
+        {
+            return services;
+        }
+
+        /// <summary>
+        /// Configures services neededed to provide the user notifications API delivery channel.
+        /// </summary>
+        /// <param name="services">The service collection.</param>
+        /// <returns>The service collection, to enable chaining.</returns>
+        public static IServiceCollection AddTenantedUserNotificationsApiDeliveryChannel(
+            this IServiceCollection services)
+        {
+            return services;
+        }
+    }
+}
---
+++ Solutions/Marain.UserNotifications.Management.Host/Marain/UserNotifications/Management/Host/Startup.cs
@@ -0,0 +1,30 @@
+﻿// <copyright file="Startup.cs" company="Endjin Limited">
+// Copyright (c) Endjin Limited. All rights reserved.
+// </copyright>
+
+[assembly: Microsoft.Azure.Functions.Extensions.DependencyInjection.FunctionsStartup(typeof(Marain.UserNotifications.Management.Host.Startup))]
+
+namespace Marain.UserNotifications.Management.Host
+{
+    using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+    using Microsoft.Extensions.DependencyInjection;
+
+    /// <summary>
+    /// Startup code for the management host.
+    /// </summary>
+    public class Startup : FunctionsStartup
+    {
+        /// <summary>
+        /// Configures the function host.
+        /// </summary>
+        /// <param name="builder">The functions host builder.</param>
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            IServiceCollection services = builder.Services;
+
+            services.AddLogging();
+
+            services.AddTenantedUserNotificationsManagementApi();
+        }
+    }
+}
