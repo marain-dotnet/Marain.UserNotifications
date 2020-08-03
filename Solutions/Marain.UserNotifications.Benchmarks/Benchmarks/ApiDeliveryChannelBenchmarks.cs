da1ce0edb9680b5e0b2d6164de98e8598e936c34
Jonathan George <jon@jonathangeorge.co.uk>
2020-08-03 09:31:18 +0100

    Basic benchmarking project (#12)
    * Added a basic benchmarking project.
    
    * Stop the benchmarks project from being packed.

---
+++ Solutions/Marain.UserNotifications.Benchmarks/Benchmarks/ApiDeliveryChannelBenchmarks.cs
@@ -0,0 +1,48 @@
+﻿// <copyright file="ApiDeliveryChannelBenchmarks.cs" company="Endjin Limited">
+// Copyright (c) Endjin Limited. All rights reserved.
+// </copyright>
+
+namespace Benchmarks
+{
+    using System;
+    using System.Net.Http;
+    using System.Net.Http.Headers;
+    using System.Threading.Tasks;
+    using BenchmarkDotNet.Attributes;
+
+    /// <summary>
+    /// Benchmarks for the API delivery channel.
+    /// </summary>
+    [JsonExporterAttribute.Full]
+    [MarkdownExporter]
+    [HtmlExporter]
+    public class ApiDeliveryChannelBenchmarks : BenchmarksBase
+    {
+        private readonly Uri apiDeliveryChannelBaseUri;
+        private string apiDeliveryChannelResourceId;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ApiDeliveryChannelBenchmarks"/> class.
+        /// </summary>
+        public ApiDeliveryChannelBenchmarks()
+        {
+            this.apiDeliveryChannelBaseUri = new Uri(this.Configuration["ApiDeliveryChannel:BaseUri"]);
+            this.apiDeliveryChannelResourceId = this.Configuration["ApiDeliveryChannel:ResourceIdForMsiAuthentication"];
+        }
+
+        /// <summary>
+        /// Benchmarks the /swagger endpoint.
+        /// </summary>
+        /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
+        [Benchmark]
+        public async Task GetSwagger()
+        {
+            HttpRequestMessage request = await this.GetHttpRequestMessageWithAuthorizationHeaderAsync(this.apiDeliveryChannelResourceId).ConfigureAwait(false);
+            request.Method = HttpMethod.Get;
+            request.RequestUri = new Uri(this.apiDeliveryChannelBaseUri, "/swagger");
+
+            HttpResponseMessage response = await this.HttpClient.SendAsync(request).ConfigureAwait(false);
+            response.EnsureSuccessStatusCode();
+        }
+    }
+}
