046d486a969704723c0ecf1bffd435728d4e18aa
Jonathan George <jon@jonathangeorge.co.uk>
2020-08-09 23:57:28 +0100

    Remove more unneeded TODOs.

--- Solutions/Marain.UserNotifications.Management.Host/Marain/UserNotifications/Management/Host/OpenApi/CreateNotificationsService.cs
+++ Solutions/Marain.UserNotifications.Management.Host/Marain/UserNotifications/Management/Host/OpenApi/CreateNotificationsService.cs
@@ -5,8 +5,10 @@
 namespace Marain.UserNotifications.Management.Host.OpenApi
 {
     using System;
+    using System.Collections.Generic;
     using System.Threading.Tasks;
     using Corvus.Tenancy;
+    using Dynamitey.DynamicObjects;
     using Marain.Operations.Client.OperationsControl;
     using Marain.Operations.Client.OperationsControl.Models;
     using Marain.Services.Tenancy;
@@ -56,15 +58,12 @@ namespace Marain.UserNotifications.Management.Host.OpenApi
             // We can guarantee tenant Id is available because it's part of the Uri.
             ITenant tenant = await this.marainServicesTenancy.GetRequestingTenantAsync(context.CurrentTenantId!).ConfigureAwait(false);
 
-            // TODO: Start long running operation
             string delegatedTenantId = await this.marainServicesTenancy.GetDelegatedTenantIdForRequestingTenantAsync(tenant.Id).ConfigureAwait(false);
             var operationId = Guid.NewGuid();
             CreateOperationHeaders response = await this.operationsControlClient.CreateOperationAsync(
                 delegatedTenantId,
                 operationId);
 
-            // TODO: Add the operation Id to the correlation Ids
-            // TODO: Start new orchestration
             IDurableOrchestrationClient orchestrationClient = context.AsDurableFunctionsOpenApiContext().OrchestrationClient
                 ?? throw new OpenApiServiceMismatchException($"Operation {CreateNotificationsOperationId} has been invoked, but no Durable Orchestration Client is available on the OpenApi context.");
 
@@ -72,7 +71,6 @@ namespace Marain.UserNotifications.Management.Host.OpenApi
                 nameof(CreateAndDispatchNotificationsOrchestration),
                 new TenantedFunctionData<CreateNotificationsRequest>(context.CurrentTenantId!, body, operationId)).ConfigureAwait(false);
 
-            // TODO: Return long running op Id.
             return this.AcceptedResult(response.Location);
         }
     }
